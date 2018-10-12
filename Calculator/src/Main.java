ca11abcde920d8a124caaa9e327aa80dbeef91c9
Stamaks <A6437149@yandex.ru>
2018-10-12 17:23:22 +0300

    Is that it?

--- Calculator/src/Main.java
+++ Calculator/src/Main.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.nio.channels.SelectableChannel;
 
 public class Main {
 
@@ -9,9 +8,10 @@ public class Main {
     private static String y = "";
     private static String xType;
     private static String yType;
-    private static String operation = "";
+    private static char operation = ' ';
 
     public static void main(String[] args) throws IOException {
+
         System.out.println("Hello! I am your simple calculator. Try smth like this:\nx = 2\ny = 3\n+");
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -21,79 +21,70 @@ public class Main {
         while (!(newLine = br.readLine()).toLowerCase().equals("exit")){
             currentTokens = newLine.split("=");
             proccess(currentTokens);
-
-            if (!x.isEmpty() && !y.isEmpty() && !operation.isEmpty())
-                perform();
         }
+
     }
 
     private static void proccess(String[] tokens) {
 
         if (tokens.length == 1) {
-            if (tokens[0].trim().length() == 1) {
-                if (!operation.isEmpty()) {
-                    System.out.println("Operation already exists!");
-                    return;
-                }
 
-                operation = tokens[0].trim();
+            if (tokens[0].trim().length() == 1) {
+
+                operation = tokens[0].trim().charAt(0);
+
+                if (!"+-*/".contains(operation + "")) {
+                    System.err.println("This operation is not implemented!");
+                    operation = ' ';
+                }
             }
             else {
-                System.out.println("Something's wrong!");
+                System.err.println("Something's wrong!");
             }
         }
 
         if (tokens.length == 2) {
-            if (tokens[0].trim().equals("x")) {
-                if (!x.isEmpty()) {
-                    System.out.println("x already exists!");
-                    return;
-                }
 
+            if (tokens[0].trim().equals("x")) {
+
+                //Проверка на то, что введенная строка после знака = это число
                 if (!(xType = getType(tokens[1].trim())).isEmpty())
                     x = tokens[1].trim();
             }
-            if (tokens[0].trim().equals("y")) {
-                if (!y.isEmpty()) {
-                    System.out.println("y already exists!");
-                    return;
-                }
 
+            if (tokens[0].trim().equals("y")) {
+
+                //Проверка на то, что введенная строка после знака = это число
                 if (!(yType = getType(tokens[1].trim())).isEmpty())
                     y = tokens[1].trim();
             }
         }
 
         if (tokens.length > 2) {
-            System.out.println("Something's wrong!");
+            System.err.println("Something's wrong!");
             return;
         }
 
-        if (!x.isEmpty() && !y.isEmpty() && !operation.isEmpty())
+        if (!x.isEmpty() && !y.isEmpty() && operation != ' ')
             perform();
     }
 
     public static void perform() {
-        String xType = getType(x), yType = getType(y);
+
         float result;
+        String xType = getType(x), yType = getType(y);
 
-        if (!xType.isEmpty() && !yType.isEmpty()) {
-            result = calculate(Float.parseFloat(x), Float.parseFloat(y));
+        result = calculate(Float.parseFloat(x), Float.parseFloat(y));
 
-            if (xType.equals("int") && yType.equals("int"))
-                System.out.println("x " + operation + " y = " + (int) result); //Округлится вниз, как и нужно.
-            else
-                System.out.println("x " + operation + " y = " + result);
-        }
-
-        //Все обнуляем
-        x = "";
-        y = "";
-        operation = "";
+        if (xType.equals("int") && yType.equals("int"))
+            System.out.println("x " + operation + " y = " + (int) result); //Округлится вниз, как и нужно.
+        else
+            System.out.println("x " + operation + " y = " + result);
     }
 
     public static float calculate(float x, float y) {
-        switch (operation.charAt(0)) {
+
+        switch (operation) {
             case '+':
                 return x + y;
             case '-':
@@ -111,20 +102,22 @@ public class Main {
     }
 
     public static String getType(String operand) {
+
         if (operand.contains(".")) {
             try {
                 Float.parseFloat(operand);
                 return "float";
             } catch (NumberFormatException e) {
-                System.err.println(e);
+                System.err.println("Number format is wrong!");
             }
-        }
-
-        try {
-            Integer.parseInt(operand);
-            return "int";
-        } catch (NumberFormatException e) {
-            System.err.println(e);
+        }   
+        else {
+            try {
+                Integer.parseInt(operand);
+                return "int";
+            } catch (NumberFormatException e) {
+                System.err.println("Number format is wrong!");
+            }
         }
 
         return "";
