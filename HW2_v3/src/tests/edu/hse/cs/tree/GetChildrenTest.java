6a832a7a09752a45246c932ac803eabe09fa3e85
Stamaks <A6437149@yandex.ru>
2018-12-08 23:20:36 +0300

    Some tests added

---
+++ HW2_v3/src/tests/edu/hse/cs/tree/GetChildrenTest.java
@@ -0,0 +1,13 @@
+package edu.hse.cs.tree;
+
+import org.junit.jupiter.api.Test;
+
+public class GetChildrenTest {
+
+    @Test
+    public void testToStringFormMutableTree() {
+//        MutableRootNode<String> root = populateTree();
+
+
+    }
+}
---
+++ HW2_v3/src/tests/edu/hse/cs/tree/ToStringFormTest.java
@@ -0,0 +1,30 @@
+package edu.hse.cs.tree;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static edu.hse.cs.tree.TreeTest.populateTree;
+
+public class ToStringFormTest {
+
+    @Test
+    public void testToStringFormMutableTree(){
+        MutableParentNode<String> root = populateTree();
+        String stringForm = root.toStringForm("");
+
+        // Проверяем, что все ноды содержатся. Не можем проверить иначе, потому что HashSet не запоминает порядок
+        Assertions.assertTrue(stringForm.contains("MutableRootNode(Root)"));
+        Assertions.assertTrue(stringForm.contains("\n    MutableParentNode(Parent0)"));
+        Assertions.assertTrue(stringForm.contains("\n        MutableChildNode(Child00)"));
+        Assertions.assertTrue(stringForm.contains("\n        MutableChildNode(Child01)"));
+        Assertions.assertTrue(stringForm.contains("\n    MutableParentNode(Parent1)"));
+        Assertions.assertTrue(stringForm.contains("\n        MutableChildNode(Child10)"));
+        Assertions.assertTrue(stringForm.contains("\n    MutableChildNode(Child0)"));
+
+        Assertions.assertFalse(stringForm.contains("\n     MutableChildNode(Child0)"));
+        Assertions.assertFalse(stringForm.contains("MutableChildNode(Child)"));
+
+
+    }
+
+}
