9f1ffc9e0d084046e3cae1e2ed17df0f74e9fb61
Stamaks <A6437149@yandex.ru>
2019-03-08 23:18:04 +0300

    Started

---
+++ HW4/src/karnaukhova/Controller.java
@@ -0,0 +1,68 @@
+package karnaukhova;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.input.DragEvent;
+
+public class Controller {
+
+    public void onMenuItemHelpAction(ActionEvent actionEvent) {
+
+    }
+
+    public void onMenuItemAuthorAction(ActionEvent actionEvent) {
+
+    }
+
+    public void onDragSwanDone(DragEvent dragEvent) {
+
+    }
+
+    public void onDragPikeDone(DragEvent dragEvent) {
+
+    }
+
+    public void onDragCrawfishDone(DragEvent dragEvent) {
+
+    }
+
+    public void onTextSBottomAction(ActionEvent actionEvent) {
+
+    }
+
+    public void onTextSTopAction(ActionEvent actionEvent) {
+
+    }
+
+    public void onTextSleepBottomAction(ActionEvent actionEvent) {
+
+    }
+
+    public void onTextSleepTopAction(ActionEvent actionEvent) {
+
+    }
+
+    public void onTextDurationAction(ActionEvent actionEvent) {
+
+    }
+
+    public void onTextWaggonXAction(ActionEvent actionEvent) {
+
+    }
+
+    public void onTextWaggonYAction(ActionEvent actionEvent) {
+
+    }
+
+    public void onStartButtonAction(ActionEvent actionEvent) {
+
+    }
+
+    public void onStopButtonAction(ActionEvent actionEvent) {
+
+    }
+
+    public void onResetButtonAction(ActionEvent actionEvent) {
+
+    }
+}
