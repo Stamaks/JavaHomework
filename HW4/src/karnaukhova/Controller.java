47bd71c3baf8b9aaf2ed0970cd1729cf917da894
Stamaks <A6437149@yandex.ru>
2019-03-08 23:24:21 +0300

    Added three fxml files

--- HW4/src/karnaukhova/Controller.java
+++ HW4/src/karnaukhova/Controller.java
@@ -2,9 +2,31 @@ package karnaukhova;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.control.Button;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.Slider;
+import javafx.scene.control.TextField;
 import javafx.scene.input.DragEvent;
 
 public class Controller {
+
+    public MenuItem menuItemHelp;
+    public MenuItem menuItemAuthor;
+    public Canvas mainCanvas;
+    public Slider sliderSwan;
+    public Slider sliderPike;
+    public Slider sliderCrawfish;
+    public TextField textSBottom;
+    public TextField textSTop;
+    public TextField textSleepBottom;
+    public TextField textSleepTop;
+    public TextField textDuration;
+    public TextField textWaggonX;
+    public TextField textWaggonY;
+    public Button buttonStart;
+    public Button buttonStop;
+    public Button buttonReset;
 
     public void onMenuItemHelpAction(ActionEvent actionEvent) {
 
